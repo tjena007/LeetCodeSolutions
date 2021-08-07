4b1388cdf266cb312cc7acd3ca6af9593f2ae7f5
Rohit Ranjan <rohitranjan0839@gmail.com>
2021-08-07 23:40:39 +0530

    461

---
+++ Bit Manipulation/461_Hamming_Distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        
+        int n = x^y;
+        int count = 0;
+        
+        while(n)
+        {
+            n = n&(n-1);
+            count++;
+        }
+        return count;
+    }
+};
